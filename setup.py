ea25af878cd3873da93fa06b9be9e2b81309bbc1
atharvapatle <atharvapatle001@gmail.com>
2025-10-21 15:28:02 +0530

    again

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+HED='-e .'
+def get_requirements(file_path):
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HED in requirements:
+            requirements.remove(HED)
+
+setup(
+    name='ml project',
+    version='0.0.1',
+    author='Atharva',
+    author_email='atharvapatle001@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+)
